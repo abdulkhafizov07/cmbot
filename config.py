41cb208d4ccf4a77ac6d46ee114e34cf2d0aac30
abdulkhafizov07 <new.jasjan.programmer@gmail.com>
2026-01-24 23:29:17 +0500

    update: using webhook to get updates

--- config.py
+++ config.py
@@ -5,6 +5,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     token: str = Field()
 
+    web_server_host: str = Field("127.0.0.1")
+    web_server_port: int = Field(8080)
+
+    webhook_path: str = Field("/webhook")
+    webhook_secret: str = Field()
+    base_webhook_url: str = Field()
+
     model_config = SettingsConfigDict(
         env_file=".env", env_prefix="cmbot_", extra="ignore"
     )
--- main.py
+++ main.py
@@ -1,19 +1,44 @@
-import asyncio
 import logging
 import sys
 
+from aiogram import Bot
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
+from aiohttp import web
+
 from application import bot, dp
+from config import settings
 from handlers import *  # noqa
 from routers import main_router
 
 
-async def main():
+async def on_startup(bot: Bot) -> None:
+    await bot.set_webhook(
+        f"{settings.base_webhook_url}{settings.webhook_path}",
+        secret_token=settings.webhook_secret,
+    )
+
+
+def main():
     logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
 
     dp.include_router(main_router)
 
-    await dp.start_polling(bot)
+    dp.startup.register(on_startup)
+
+    app = web.Application()
+
+    webhook_requests_handler = SimpleRequestHandler(
+        dispatcher=dp,
+        bot=bot,
+        secret_token=settings.webhook_secret,
+    )
+
+    webhook_requests_handler.register(app, path=settings.webhook_path)
+
+    setup_application(app, dp, bot=bot)
+
+    web.run_app(app, host=settings.web_server_host, port=settings.web_server_port)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
